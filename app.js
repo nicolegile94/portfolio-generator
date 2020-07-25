96e56932ba0edb540d717d725adfdd10d197121f
Nicole Gile <nicolegile94@gmail.com>
2020-07-25 14:04:01 -0500

    initial features

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArgs => {
-    for (let i=0; i < profileDataArgs.length; i += 1) {
-        console.log(profileDataArgs[i]);
-    }
-
-    console.log('============')
-
-    profileDataArgs.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+  fs.writeFile('index.html', generatePage(name, github), err => {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
